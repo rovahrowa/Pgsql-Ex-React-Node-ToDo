e3a0f6147d252573b0af9fc5289329d3e1fc190a
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-10 15:43:26 +0300

    Express server with requred npm packages

---
+++ bin/www.js
@@ -0,0 +1,12 @@
+/**
+ * Created by danstan on 5/10/17.
+ */
+// This will be our application entry. We'll setup our server here.
+const http = require('http');
+const app = require('../app'); // The express app we just created
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
