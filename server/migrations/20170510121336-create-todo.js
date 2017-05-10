30add6d19ab22b6895c1888f34ce1ae8855af7f4
Danstan Otieno Onyango <otis.eng.555@gmail.com>
2017-05-10 15:54:38 +0300

    Coverts the code to ECS2016

--- server/migrations/20170510121336-create-todo.js
+++ server/migrations/20170510121336-create-todo.js
@@ -1,27 +1,24 @@
-'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Todos', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Todos');
-  }
+    up: (queryInterface, Sequelize) =>
+        queryInterface.createTable('Todos', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        }),
+    down: (queryInterface /* , Sequelize */) => queryInterface.dropTable('Todos'),
 };
--- server/migrations/20170510121531-create-todo-item.js
+++ server/migrations/20170510121531-create-todo-item.js
@@ -1,30 +1,38 @@
-'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('TodoItems', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      content: {
-        type: Sequelize.STRING
-      },
-      complete: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('TodoItems');
-  }
+    up: (queryInterface, Sequelize) =>
+        queryInterface.createTable('TodoItems', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            content: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            complete: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            todoId: {
+                type: Sequelize.INTEGER,
+                onDelete: 'CASCADE',
+                references: {
+                    model: 'Todos',
+                    key: 'id',
+                    as: 'todoId',
+                },
+            },
+        }),
+    down: (queryInterface /* , Sequelize */) =>
+        queryInterface.dropTable('TodoItems'),
 };
